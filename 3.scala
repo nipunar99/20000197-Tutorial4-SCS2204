dd5615e6cbe08921e332c006612d4cb69af4b4b6
nipunar99 <2020cs019@stu.ucsc.cmb.ac.lk>
2022-08-01 22:30:46 +0530

    Final final

--- 3.scala
+++ 3.scala
@@ -1,22 +1,6 @@
 object main extends App{
     def toUpper(s:String):String=s.toUpperCase();
     def toLower(s:String):String=s.toLowerCase();
-    def toSpecialStyle(s:String)(indexes: Int*):String={
-        var i:Int =0;
-        var j:Int =0;
-        var result:String="";
-        for(i <- 0 to (s.length()-1)){
-            var temp:String = s.charAt(i).toString();
-            for(j<-0 to (indexes.length-1)){
-                if(indexes(j)==i){
-                    temp=toUpper(temp);
-                }
-            }
-            result=result+temp;
-        }
-        result;
-    }
-
 
     def formatNames(name: String)(func: String => String)(indexes: Int*):String = {
         if (indexes.isEmpty){ 
