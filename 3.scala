2cb342e9002a506a5d2731271b89e0a1e46b82c6
nipunar99 <2020cs019@stu.ucsc.cmb.ac.lk>
2022-08-01 20:37:19 +0530

    question 3 answer half

--- 3.scala
+++ 3.scala
@@ -3,5 +3,9 @@ object main extends App{
     def toUpper(s:String):String=s.toUpperCase();
     def toLower(s:String):String=s.toLowerCase();
 
+    def formatNames(name: String)(func: String => String):String = {
+        func(name);
+    }
+
     println(toUpper("hello"))
 }
